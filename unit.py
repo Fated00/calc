827303b32655d78d4a71d87cf29d72ba57655e07
Fated00 <114770655+Fated00@users.noreply.github.com>
2022-12-23 00:31:11 +0300

    Update unit.py

--- unit.py
+++ unit.py
@@ -20,7 +20,7 @@ class TestCalculator(unittest.TestCase):
     self.assertEqual(div(120,6), 20)
   def test_mod(self):
 <<<<<<< HEAD
-    self.assertEqual(modd(100,10),0)
+    self.assertEqual(modd(100,10),450)
 =======
     self.assertEqual(modd(110,10),70)
 >>>>>>> master
